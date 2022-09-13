96aac86e68db09c2d8cff40c8e55a0644fdaf158
rfcniblon <51241155+rfcniblon@users.noreply.github.com>
2022-09-13 15:30:28 +0200

    correction bug xcopy

--- Backup to Usb/backup.bat
+++ Backup to Usb/backup.bat
@@ -1,7 +1,7 @@
 @echo off
 echo Check les disques dur pour savoir lequel est le bon .
 set FILENAME=.\text.txt
-set DRIVERLIST=F,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z
+set DRIVERLIST=D,E,F,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z
 FOR %%d in (%DRIVERLIST%) do (
 FOR /F "delims=" %%f in ('dir /B /S %%d:\%FILENAME%') do (
 set LETTER=%%d
@@ -12,7 +12,8 @@ IF EXIST "%%d:\%FILENAME%" goto TRAITEMENT
 echo Fichier trouve dans le lecteur %LETTER% !
 echo la sauvegarde a commencer !
 echo.
-xcopy F:\"text" %LETTER%:\  /E /R /Y /I /D  
+xcopy %LETTER%:\ F:\ /E /R /Y /I /D 
+rem xcopy F:\"text" %LETTER%:\  /E /R /Y /I /D  
 rem xcopy C:\"test" J:\tc\"test"  /E /H /R /Y /I /D >>Log.txt   
 echo.
 echo la sauvegarde est fini !
